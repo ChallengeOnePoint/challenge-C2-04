d7a2a011d3cae2bbf5e151ef69872a54d016fbb1
gregoryRednet <gregory.bernard@rednet.io>
2016-01-23 15:18:06 +0100

    manager big and small viewws

--- public/application.js
+++ public/application.js
@@ -29,8 +29,18 @@ function kittenFactory ($http) {
 function AppController (Kitten) {
   this.class   = 'display-small';
   this.kittens = Kitten.load();
+  this.photoClass = 'col-sm-6 col-md-4';
 }
 
 AppController.prototype.display = function (mode) {
   this.class = 'display-' + mode;
+  if (mode === 'big') {
+    this.photoClass = 'col-sm-6 col-md-4';
+  }
+  else if (mode === 'small') {
+    this.photoClass = 'col-sm-4 col-md-2';
+  }
+  else {
+    //list view
+  }
 };
