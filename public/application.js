28ad51a4e102b58d2754f78ece06d60f689704ab
Jean Dupouy <jean.dupouy@rednet.io>
2016-01-23 14:51:57 +0100

    mvp

--- public/application.js
+++ public/application.js
@@ -2,8 +2,30 @@
 
 angular
   .module('app', [])
+  .factory('Kitten', kittenFactory)
   .controller('AppController', AppController);
 
-function AppController () {
-  console.log('hello world');
+function kittenFactory ($http) {
+  var KITTEN_URL = '/cats/kittenIdentity.json';
+
+  return {
+    kittens: angular.extend([], { $resolved: false }),
+    load: load
+  };
+
+  function load () {
+    var self = this;
+
+    $http.get(KITTEN_URL).then(function (res) {
+      self.kittens.length = 0;
+      self.kittens.push.apply(self.kittens, res.data);
+      self.kittens.$resolved = true;
+    });
+
+    return self.kittens;
+  }
+}
+
+function AppController (Kitten) {
+  this.kittens = Kitten.load();
 }
